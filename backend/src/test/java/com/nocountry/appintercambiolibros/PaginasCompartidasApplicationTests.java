9f9886dbc039aa57557a4d734315956398e951e1
matiasperlo <matias_perlo_arg@outlook.es>
2024-02-13 22:37:40 -0300

    initial commit - backend

---
+++ backend/src/test/java/com/nocountry/appintercambiolibros/PaginasCompartidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.appintercambiolibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginasCompartidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
