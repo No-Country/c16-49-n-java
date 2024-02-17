e6a8ba2cc66ef5dc0fe3661ea95919709e22886b
matiasperlo <matias_perlo_arg@outlook.es>
2024-02-17 18:46:34 -0300

    Renombrar Controller en singular y url en plural

--- backend/src/main/java/com/nocountry/appintercambiolibros/controller/ImagenController.java
+++ backend/src/main/java/com/nocountry/appintercambiolibros/controller/ImagenController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +17,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.nocountry.appintercambiolibros.services.ImagenService;
 
 @RestController()
-@RequestMapping("api/imagen")
-public class ImagenesController {
+@RequestMapping("imagenes")
+public class ImagenController {
 
     @Autowired 
     ImagenService imagenService;
