e1b56e03aba24c8977c226fa3872103a640d925a
matiasperlo <matias_perlo_arg@outlook.es>
2024-02-24 12:46:28 -0300

    BUGFIX: quitar codigo comentado en Application

--- backend/src/main/java/com/nocountry/appintercambiolibros/PaginasCompartidasApplication.java
+++ backend/src/main/java/com/nocountry/appintercambiolibros/PaginasCompartidasApplication.java
@@ -1,17 +1,8 @@
 package com.nocountry.appintercambiolibros;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-/*@EnableAutoConfiguration( exclude = {
-	DataSourceAutoConfiguration.class,
-	HibernateJpaAutoConfiguration.class,
-	SecurityAutoConfiguration.class
-})*/
 @SpringBootApplication
 public class PaginasCompartidasApplication {
 
