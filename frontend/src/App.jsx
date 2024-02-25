2ee750f609e037fcd5aedf3066d8ced46720f6fa
MAITAYORMARIS <yormarism@gmail.com>
2024-02-25 14:04:01 -0300

    link en app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,9 +24,9 @@ function App() {
 
   useEffect(() => {
     axios
-      .get('http://localhost:8080/api/v1/libros')
+      .get('http://localhost:8080/api/v1/libros/buscar')
       .then((response) => {
-       console.log(response)
+       
         setDataLibros(response.data);
         setLoading(false);
       })
