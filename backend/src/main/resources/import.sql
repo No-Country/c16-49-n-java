af9a16ff6cef7e87294b69dfb69922ae911e2b13
matiasperlo <matias_perlo_arg@outlook.es>
2024-03-06 00:39:45 -0300

    UPDATE: nuevos datos de prueba: reseñas, comentarios

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,13 +1,13 @@
 --USUARIOS
-INSERT INTO usuarios (nombre, email, fecha_creacion, nombre_imagen, password) VALUES ('frankenstein', 'nstei@gmail.com', NOW() , 'frank1.jpg', 'sh@d0w231');
-INSERT INTO usuarios (nombre, email, fecha_creacion, nombre_imagen, password) VALUES ('Rambo', 'rambo@gmail.com', NOW() , 'frank1.jpg', 'H8lH,o5C1e');
-INSERT INTO usuarios (nombre, email, fecha_creacion, nombre_imagen, password) VALUES ('PlateBible', 'eBibl@gmail.com', NOW() , 'frank1.jpg', 'ej31ds');
-INSERT INTO usuarios (nombre, email, fecha_creacion, nombre_imagen, password) VALUES ('BlueAlto', 'ueAlto@gmail.com', NOW() , 'frank1.jpg', '24@fe');
-INSERT INTO usuarios (nombre, email, fecha_creacion, nombre_imagen, password) VALUES ('ProteinDolphin', 'einDolphi@gmail.com', NOW() , 'frank1.jpg', '!!reee32');
-INSERT INTO usuarios (nombre, email, fecha_creacion, nombre_imagen, password) VALUES ('SymbolicLovecraft', 'mboli@gmail.com', NOW() , 'frank1.jpg', 'roma-12s');
-INSERT INTO usuarios (nombre, email, fecha_creacion, nombre_imagen, password) VALUES ('SunrisePassion', 'assi@gmail.com', NOW() , 'frank1.jpg', 'elvis1M');
-INSERT INTO usuarios (nombre, email, fecha_creacion, nombre_imagen, password) VALUES ('NickelStability', 'tability@gmail.com', NOW() , 'frank1.jpg', 'cattt1');
-INSERT INTO usuarios (nombre, email, fecha_creacion, nombre_imagen, password) VALUES ('MacroBook', 'macro@gmail.com', NOW() , 'frank1.jpg', 's0meth!ng');
+INSERT INTO usuarios (nombre, email, fecha_creacion, password, role) VALUES ('frankenstein', 'nstei@gmail.com', NOW() , '$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C', 'USUARIO');
+INSERT INTO usuarios (nombre, email, fecha_creacion, password, role) VALUES ('Rambo', 'rambo@gmail.com', NOW() , '$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C', 'USUARIO');
+INSERT INTO usuarios (nombre, email, fecha_creacion, password, role) VALUES ('PlateBible', 'eBibl@gmail.com', NOW() , '$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C', 'USUARIO');
+INSERT INTO usuarios (nombre, email, fecha_creacion, password, role) VALUES ('BlueAlto', 'ueAlto@gmail.com', NOW() , '$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C', 'USUARIO');
+INSERT INTO usuarios (nombre, email, fecha_creacion, password, role) VALUES ('ProteinDolphin', 'einDolphi@gmail.com', NOW() , '$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C', 'USUARIO');
+INSERT INTO usuarios (nombre, email, fecha_creacion, password, role) VALUES ('SymbolicLovecraft', 'mboli@gmail.com', NOW() , '$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C', 'USUARIO');
+INSERT INTO usuarios (nombre, email, fecha_creacion, password, role) VALUES ('SunrisePassion', 'assi@gmail.com', NOW() , '$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C', 'USUARIO');
+INSERT INTO usuarios (nombre, email, fecha_creacion, password, role) VALUES ('NickelStability', 'tability@gmail.com', NOW() , '$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C', 'USUARIO');
+INSERT INTO usuarios (nombre, email, fecha_creacion, password, role) VALUES ('MacroBook', 'macro@gmail.com', NOW() , '$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C', 'USUARIO');
 
 --ADMINISTRADORES
 INSERT INTO usuarios (nombre, email, password, role, fecha_creacion) VALUES ('Pablo', 'pablo@gmail.com','$2a$10$5kRDQSmUeUDoroBwx.s4K.2avo38jq.XVS8NWfSR2I3wsEqJmTu9C','ADMINISTRADOR', NOW());
@@ -29,6 +29,39 @@ INSERT INTO libros (isbn, usuario_id,  titulo, autor, fecha_de_publicacion, resu
 --RESEÑAS
 INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (4.0, 1, 1)
 INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (2.0, 1, 7)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (3.0, 1, 2)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (4.0, 1, 3)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (5.0, 1, 4)
 INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (1.0, 2, 2)
 INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (2.0, 3, 6)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (3.7, 3, 2)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (4.0, 3, 4)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (3.5, 3, 5)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (3.0, 5, 1)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (4.0, 5, 2)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (3.0, 5, 4)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (4.0, 5, 5)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (2.0, 5, 6)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (3.0, 5, 7)
+INSERT INTO resenias (calificacion, libro_id, usuario_id) VALUES (4.0, 5, 8)
+
+
+--COMENTARIOS
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 1, 1, 'Lo recomiendo para introducirse al género')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 7, 1, 'No me convenció del todo')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 2, 1, 'Me gustó, pero esperaba más')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 3, 1, 'Una lectura entretenida')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 4, 1, 'Excelente libro, lo recomiendo totalmente')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 2, 2, 'No lo recomiendo')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 6, 3, 'Le falta mejorar')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 2, 3, 'Me gustó la historia pero algunos giros fueron predecibles')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 4, 3, 'Personajes bien desarrollados y una trama que te atrapa desde el principio')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 5, 3, 'Una lectura interesante, aunque esperaba un poco más de acción')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 1, 5, 'Muy intrigante, me mantuvo enganchado hasta el final')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 2, 5, 'Una trama bien desarrollada y personajes interesantes')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 4, 5, 'Me gustó mucho, lo recomendaría sin dudarlo')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 5, 5, 'Excelente libro, lo disfruté de principio a fin')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 6, 5, 'No me convenció del todo, esperaba más')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 7, 5, 'Entretenido pero predecible en algunos aspectos')
+INSERT INTO comentarios (fecha_de_creacion, usuario_id, libro_id, contenido) VALUES (NOW(), 8, 5, 'Uno de los mejores libros que he leído en mucho tiempo')
 
